1632e1a5490da2aab9add093af205910245d0842
zhengguang <zw4re@virginia.edu>
2023-04-22 22:42:33 -0400

    greyscale, split done

--- ReadImages.py
+++ ReadImages.py
@@ -1,14 +1,23 @@
 from PIL import Image
 import os
+import numpy as np 
+from sklearn.model_selection import train_test_split
 # Set the path to the folders containing the images and labels
 
 class ReadImages():
-    ##"/Users/zhengguang/Desktop/OneDrive - University of Virginia/Desktop/CS 4774/Brain-Tumoer-Classification/archive"
+    #"/Users/zhengguang/Desktop/OneDrive - University of Virginia/Desktop/CS 4774/Brain-Tumoer-Classification/archive"
     def __init__(self,parent_folder_path):
         self.parent_folder_path = parent_folder_path
         self.images = []
         self.labels = []
         self.label_map = {"glioma_tumor": 0, "meningioma_tumor": 1, "no_tumor": 2, "pituitary_tumor": 3}
+        self.training_images=None
+        self.training_labels=None
+        self.testing_images=None
+        self.testing_labels=None
+        self.validation_images=None
+        self.validation_labels=None
+
     # Loop through each subfolder and read in the images and labels
     def Reading(self):
         for subfolder in os.listdir(self.parent_folder_path):
@@ -28,10 +37,24 @@ class ReadImages():
                         continue  # skip hidden files and folders
                     if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
                         img = Image.open(os.path.join(folder_path, file))
-                        self.images.append(img)
+                        img=img.convert("L")
+                        img_array = np.array(img).flatten()
+                        self.images.append(img_array)
                         label_str = folder
                         label = self.label_map[label_str]
                         self.labels.append(label)
 
 
-
+    def Split(self):
+        self.training_images, X_test_val, self.training_labels, y_test_val = train_test_split(self.images, self.labels, test_size=0.2, random_state=42)
+        self.testing_images,self.testing_labels,self.validation_images,self.validation_labels=train_test_split(
+            X_test_val, y_test_val, test_size=0.1, random_state=42)
+        
+    def get_training(self):
+        return self.testing_images,self.training_labels
+    
+    def get_testing(self):
+        return self.training_images,self.training_labels
+    
+    def get_validation(self):
+        return self.validation_images,self.validation_labels
--- main.py
+++ main.py
@@ -2,4 +2,8 @@ from ReadImages import ReadImages
 
 Factory=ReadImages("/Users/zhengguang/Desktop/OneDrive - University of Virginia/Desktop/CS 4774/Brain-Tumoer-Classification/archive")
 Factory.Reading()
-print(Factory.labels)
+Factory.Split()
+print("finished")
+Factory.get_training()
+Factory.get_testing()
+Factory.get_validation()
