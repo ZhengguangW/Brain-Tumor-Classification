9e0b9f853d7963b92d7ede40717c5661b14c061e
MoFololo <116032908+MoFololo@users.noreply.github.com>
2023-04-23 20:48:54 -0400

    svm

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 from ReadImages import ReadImages
+from sklearn.svm import SVC
 
-Factory=ReadImages("/Users/zhengguang/Desktop/OneDrive - University of Virginia/Desktop/CS 4774/Brain-Tumoer-Classification/archive")
+# Reading Data from file
+Factory = ReadImages("/Users/david_da_lian/Downloads/archive")
 Factory.Reading()
 Factory.Split()
 print("finished")
-X_train,y_train=Factory.get_training()
+
+# Splitting Data in to 811
+X_train, y_train = Factory.get_training()
+X_test, y_test = Factory.get_testing()
+X_val, y_val = Factory.get_validation()
 print(len(X_train))
 print(len(y_train))
-X_test,y_test=Factory.get_testing()
 print(X_train.shape)
-Factory.get_validation()
-clf = MLPClassifier(hidden_layer_sizes=(64,), activation='relu', solver='adam', random_state=42)
-clf.fit(X_train,y_train)
+
+# Build First Classifier (MLPC) --------------------------------------------------
+print('MLPC:')
+clf_mlpc = MLPClassifier(hidden_layer_sizes=(64,), activation='relu', solver='adam', random_state=42)
+clf_mlpc.fit(X_train, y_train)
+
+# Evaluate the classifier on the training data
+print("Training Accuracy: {}".format(clf_mlpc.score(X_train, y_train)))
+
+# Evaluate the classifier on the validation data
+print("Validation Accuracy: {}".format(clf_mlpc.score(X_val, y_val)))
 
 # Evaluate the classifier on the testing data
-score = clf.score(X_test, y_test)
-print("Accuracy:", score)
+print("Testing Accuracy: {}".format(clf_mlpc.score(X_test, y_test)))
+
+# Build Second Classifier (SVM) --------------------------------------------------
+print('SVM:')
+clf_svm = SVC(kernel='rbf', C=1)
+clf_svm.fit(X_train, y_train)
+
+# Evaluate the classifier on the training data
+print("Training Accuracy: {}".format(clf_svm.score(X_train, y_train)))
+
+# Evaluate the classifier on the testing data
+print("Testing Accuracy: {}".format(clf_svm.score(X_test, y_test)))
