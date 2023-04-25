4e8316c69e50b9c3df4d2fb1348ec8cb3475088b
MoFololo <116032908+MoFololo@users.noreply.github.com>
2023-04-25 14:22:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,3 +41,38 @@ print("Training Accuracy: {}".format(clf_svm.score(X_train, y_train)))
 
 # Evaluate the classifier on the testing data
 print("Testing Accuracy: {}".format(clf_svm.score(X_test, y_test)))
+
+# Find Better hyperparamerter for SVM --------------------------------------------------
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+
+
+# define the parameter grid
+param_grid = {'C': [0.1, 1, 10],
+              'kernel': ['rbf', 'linear', 'poly', 'sigmoid']}
+
+# instantiate an SVM classifier
+svc = SVC()
+
+# instantiate the grid search with a cross-validation of 5
+grid_search = GridSearchCV(svc, param_grid, cv=5)
+
+# fit the grid search to the training data
+grid_search.fit(X_train, y_train)
+
+# select the best hyperparameters
+best_params = grid_search.best_params_
+
+# train the model on the entire training set using the best hyperparameters
+clf = SVC(**best_params)
+clf.fit(X_val, y_val)
+
+# evaluate the performance on the validation set
+y_pred_val = clf.predict(X_val)
+val_accuracy = accuracy_score(y_val, y_pred_val)
+print("Validation Accuracy:", val_accuracy)
+
+# evaluate the performance on the test set
+y_pred_test = clf.predict(X_test)
+test_accuracy = accuracy_score(y_test, y_pred_test)
+print("Test Accuracy:", test_accuracy)
